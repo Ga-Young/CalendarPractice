07f5b3801e49f8c5251e09113afad214add87856
Ga-Young <37060421+Ga-Young@users.noreply.github.com>
2020-09-10 04:19:56 +0900

    swipe to next
    ㅇㅇ

--- app/src/main/java/com/example/recycleviewtest/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewtest/MainActivity.kt
@@ -3,22 +3,26 @@ package com.example.recycleviewtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashSet
 
 class MainActivity : AppCompatActivity() {
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //옆으로 넘기기
+        val snapHelper : SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+
         //어댑터 등록
         recyclerView.adapter = RecyclerviewAdapter(setData())
+
     }
 
     //data 생성
